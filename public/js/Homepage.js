c2e77e2d8a852beca5bea8dec2157287760725af
Mikey J <mikeylashaun@icloud.com>
2021-06-29 13:17:06 -0700

    finished api for keyword searchbar

--- public/js/Homepage.js
+++ public/js/Homepage.js
@@ -1,31 +1,35 @@
 // Formating the search
-// if(data.indexOf(" ") > 0) {
-//     let theData = data.replace(/\s/g, '%20')
-//     console.log(theData)
-//     searchLocation(theData);
-//   } else {
-//     searchLocation(city);
-//   }
+function properFormat(data) {
+    if (data.indexOf(" ") > 0) {
+        let theData = data.replace(/\s/g, '%20')
+        console.log(theData)
+        return theData;
+    } else {
+        return data;
+    }
+}
+
 
 async function searchBar(data) {
-
-  
-
-    const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?size=10&keyword=${data}}&apikey=y7jtPwcsLI955aEToVqLFC7r53xG1Umr`, {
-      method: "GET",
-      body: {
-        "genreId": "KnvZfZ7vAde",
-        "apiKey": "y7jtPwcsLI955aEToVqLFC7r53xG1Umr"
-      }
+    try{
+    const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?size=10&keyword=${properFormat(data)}}&apikey=y7jtPwcsLI955aEToVqLFC7r53xG1Umr`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        }, 
     });
     if (response.ok) {
-      console.log(response);
+        console.log(response);
     } else {
-      alert("Nothing to search");
+        alert("Nothing to search");
     }
     return response;
-  };
-  
+    } catch (err) {
+        console.log(err); 
+        res.status(500).json(err); 
+    }
+};
+
   // fetch("https://ticketmasterstefan-skliarovv1.p.rapidapi.com/getSingleGenre", {
   // 	"method": "GET",
   // 	"body": {
