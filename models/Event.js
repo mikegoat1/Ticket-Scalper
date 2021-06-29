d19f56f4c205968667292974ee08d02028dd5ae5
chrischo94 <82658603+chrischo94@users.noreply.github.com>
2021-06-30 05:15:02 +0900

    rough draft of models

---
+++ models/Event.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Event extends Model {}
+
+Event.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    venue: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    segment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    genre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subGenre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+    price_range: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'event',
+  }
+);
+
+module.exports = Event;
