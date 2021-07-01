924aa366c8a6942289ccaeab2d34c8dc52b41e67
chrischo94 <82658603+chrischo94@users.noreply.github.com>
2021-07-01 18:23:50 +0900

    event route controller

---
+++ controllers/api/eventRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const { Event } = require('../../models');
+
+
+//POST Event Values
+router.post('/', async (req, res) => {
+    try {
+      console.log(req.body)
+      const eventData = await Event.create({
+        ...req.body,
+        // user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(eventData);
+      console.log(eventData)
+    } catch (err) {
+      res.status(400).json(err);
+    }
+});
+  
+
+module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -48,27 +48,34 @@ router.get('/event/:id', async (req, res) => {
         ],
       });
       const events = eventData.get({ plain: true });
-      console.log(events); 
-      res.render('detailResult', { events, loggedIn: req.session.loggedIn });
-    } catch (err) {
-      console.log(err);
+      res.render('detailResult', {
+        ...events,
+        logged_in: req.session.logged_in
+      });
+      } catch (err) {
       res.status(500).json(err);
-    }
-  }
-});
+      }
+      };
+    });
 
-//POST Event Values
-router.post('/event', async (req, res) => {
+
+// POST Event Values
+router.get('/event', async (req, res) => {
   try {
     console.log(req.body)
-    const eventData = await Event.create(req.body);
-      
+    const eventData = await Event.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
 
     res.status(200).json(eventData);
+    console.log(eventData)
   } catch (err) {
     res.status(400).json(err);
   }
 });
+
+
 // GET profile
 router.get('/profile', withAuth, async (req, res) => {
   try {
