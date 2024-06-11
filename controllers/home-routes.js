ed871c8d7219c549bdfbf8bc21e145ddce3ddff8
mikegoat1 <mikeg.o.a.t.1@gmail.com>
2024-06-11 15:30:18 -0700

    checking to see if I still have connection via remote

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -87,7 +87,7 @@ router.get('/detailResult/:id', async (req, res) => {
 //   try {
 //     console.log(req.body)
 //     const eventData = await Event.create(req.body);
-      
+
 
 //     res.status(200).json(eventData);
 //   } catch (err) {
